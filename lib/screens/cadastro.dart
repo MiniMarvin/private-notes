375d7c1658ca2cb36a861f4fa4c3eecc1afd3ebb
Caio Gomes <caiogomes34@gmail.com>
2021-02-02 23:12:54 -0300

    feat: back button cadastro
    O cadastro foi modificado para possuir
    um botão de retorno e ter a UI mais
    folcada em espaço.

--- lib/screens/cadastro.dart
+++ lib/screens/cadastro.dart
@@ -13,100 +13,115 @@ class _CadastroState extends State<Cadastro> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        debugPrint('tap');
-        FocusScope.of(context).unfocus();
-      },
-      child: BaseLoginScreen(
-        child: Form(
-          key: _formKey,
-          child: Column(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('cadastro'),
+        elevation: 0,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).buttonColor,
+        ),
+        centerTitle: false,
+      ),
+      body: GestureDetector(
+        onTap: () {
+          debugPrint('tap');
+          FocusScope.of(context).unfocus();
+        },
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+          child: ListView(
             children: <Widget>[
-              Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                  child: Row(
-                    children: [
-                      Image.asset('assets/images/addImage.png'),
-                      Padding(
-                        child: Container(
-                          child: Text(
-                            'adicione uma foto ao seu perfil',
-                            textAlign: TextAlign.center,
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                        child: Row(
+                          children: [
+                            Image.asset('assets/images/addImage.png'),
+                            Padding(
+                              child: Container(
+                                child: Text(
+                                  'adicione uma foto ao seu perfil',
+                                  textAlign: TextAlign.center,
+                                ),
+                                width: MediaQuery.of(context).size.width * 0.5 -
+                                    60,
+                              ),
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.start,
+                        )),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20, bottom: 5),
+                      child: Row(
+                        children: [
+                          Text(
+                            'email',
+                            style: Theme.of(context).textTheme.bodyText1,
                           ),
-                          width: MediaQuery.of(context).size.width * 0.5 - 60,
-                        ),
-                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.start,
                       ),
-                    ],
-                    mainAxisAlignment: MainAxisAlignment.start,
-                  )),
-              Padding(
-                padding: EdgeInsets.only(top: 20, bottom: 5),
-                child: Row(
-                  children: [
-                    Text(
-                      'email',
-                      style: Theme.of(context).textTheme.bodyText1,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: 'seu@email.com',
+                      ),
+                      validator: (value) {
+                        var regex = RegExp(
+                          r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$",
+                        );
+                        final v = regex.allMatches(value).isEmpty;
+                        debugPrint(v.toString());
+                        if (regex.allMatches(value).isEmpty) {
+                          return 'email inválido';
+                        }
+                        return null;
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 40, bottom: 5),
+                      child: Row(
+                        children: [
+                          Text(
+                            'senha',
+                            style: Theme.of(context).textTheme.bodyText1,
+                          ),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.start,
+                      ),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: '******',
+                      ),
+                      validator: (value) {
+                        if (value.length < 6) {
+                          return 'A senha precisa de pelo menos 6 caracteres';
+                        }
+                        return null;
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 50, bottom: 10),
+                      child: SizedBox(
+                        width: double.infinity,
+                        child: RaisedButton(
+                          onPressed: _cadastrar,
+                          child: Text('cadastrar'),
+                          elevation: 0,
+                        ),
+                      ),
                     ),
                   ],
-                  mainAxisAlignment: MainAxisAlignment.start,
-                ),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'seu@email.com',
-                ),
-                validator: (value) {
-                  var regex = RegExp(
-                    r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$",
-                  );
-                  final v = regex.allMatches(value).isEmpty;
-                  debugPrint(v.toString());
-                  if (regex.allMatches(value).isEmpty) {
-                    return 'email inválido';
-                  }
-                  return null;
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 40, bottom: 5),
-                child: Row(
-                  children: [
-                    Text(
-                      'senha',
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.start,
-                ),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  hintText: '******',
-                ),
-                validator: (value) {
-                  if (value.length < 6) {
-                    return 'A senha precisa de pelo menos 6 caracteres';
-                  }
-                  return null;
-                },
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 50, bottom: 10),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: RaisedButton(
-                    onPressed: _cadastrar,
-                    child: Text('cadastrar'),
-                    elevation: 0,
-                  ),
                 ),
               ),
             ],
           ),
         ),
-        title: 'o seu\ndiário secreto',
       ),
     );
   }
